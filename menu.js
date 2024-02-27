d3ef8a146ee65669128dba1b8f346fe21d0d5c8e
Fl3xTape <77446594+Fl3xTape@users.noreply.github.com>
2024-02-27 16:22:04 +0100

    Implementing my recent changes too all pages.

---
+++ menu.js
@@ -0,0 +1,11 @@
+function openMenu() 
+{
+    document.getElementById('navigation').style.right='0%';
+    document.getElementById('menu').style.opacity='1';
+}   
+
+function closeMenu() 
+{
+    document.getElementById('navigation').style.right='-100%';
+    document.getElementById('menu').style.opacity='0';
+}    
