3f35af5bbb698978701863a966f72e2f1c2505b4
Strike6782 <ivanbalkenende+gh@gmail.com>
2024-03-08 13:01:07 +0100

    Change menu to come from the left

--- menu.js
+++ menu.js
@@ -1,12 +1,12 @@
 function openMenu() 
 {
-    document.getElementById('navigation').style.right='0%';
+    document.getElementById('navigation').style.left='0%';
     document.getElementById('menu').style.opacity='1';
 }   
 
 function closeMenu() 
 {
-    document.getElementById('navigation').style.right='-100%';
+    document.getElementById('navigation').style.left='-100%';
     document.getElementById('menu').style.opacity='0';
 }
 
