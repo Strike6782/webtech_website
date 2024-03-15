f20bf960d946d2c217b1f4564aa494f26451d99c
Fl3xTape <77446594+Fl3xTape@users.noreply.github.com>
2024-03-15 12:12:45 +0100

    popup follows cursor

--- popup.js
+++ popup.js
@@ -1,17 +1,16 @@
-
-
-
-
-
-
-
-
-
 var authorImage = document.getElementById('author-pic');
 var popupBox = document.getElementById('popup-box');
 
 
+function updateDisplay(event) {
+  popupBox.style.left = event.pageX + 'px';
+  popupBox.style.top = event.pageY + 'px';
+
+  console.log(event.pageX + 'px', event.pageY + 'px'); 
+} 
+
 authorImage.addEventListener('mouseover', function(){
+  popupBox.style.position='fixed';
   popupBox.style.display='block';
 });
 
@@ -19,6 +18,9 @@ authorImage.addEventListener('mouseout', function(){
   popupBox.style.display='none';
 });
 
+authorImage.addEventListener("mousemove", updateDisplay, false);
+authorImage.addEventListener("mouseenter", updateDisplay, false);
+authorImage.addEventListener("mouseleave", updateDisplay, false);
 
 
 
