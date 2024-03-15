2bdedad1b9936afab7fe69a6a427ec777a8327ad
Strike6782 <ivanbalkenende+gh@gmail.com>
2024-03-15 13:44:28 +0100

    changed minor appearance popup and reverted popup changes

--- popup.js
+++ popup.js
@@ -1,27 +1,13 @@
 var authorImage = document.getElementById('author-pic');
 var popupBox = document.getElementById('popup-box');
 
-
-function updateDisplay(event) {
-  popupBox.style.left = event.pageX + 'px';
-  popupBox.style.top = event.pageY + 'px';
-
-  console.log(event.pageX + 'px', event.pageY + 'px'); 
-} 
-
 authorImage.addEventListener('mouseover', function(){
-  popupBox.style.position='fixed';
   popupBox.style.display='block';
 });
 
 authorImage.addEventListener('mouseout', function(){
   popupBox.style.display='none';
 });
-
-authorImage.addEventListener("mousemove", updateDisplay, false);
-authorImage.addEventListener("mouseenter", updateDisplay, false);
-authorImage.addEventListener("mouseleave", updateDisplay, false);
-
 
 
 
